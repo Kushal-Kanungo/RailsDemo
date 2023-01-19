a09d59d6d3778f7839a96f73e3e060162a2f7a8e
Kushal <kushal.kanungo@metacube.com>
2023-01-19 10:24:49 +0530

    Added Encode Token Method in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  # It will encode the payload
+  # We created in 'Application Controller' so it will be available to all controller inheriting it
+  def encode_token(payload)
+    JWT.encode(payload, 'secret')
+  end
 end
