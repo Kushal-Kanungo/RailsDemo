2681829f1da52d992f2bea4d088016f03833e7b6
Kushal <kushal.kanungo@metacube.com>
2023-01-19 11:41:31 +0530

    Create a login action in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  def create
+  def register
     @user = User.create(user_params)
     if @user.valid?
       token = encode_token({ user_id: @user.id })
@@ -9,9 +9,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def login
+    @user = User.find_by(username: user_params_login[:username])
+
+    # authenticate method from bycrpt
+    # same as @user && authenticate(user_params_login[:password])
+    if @user&.authenticate(user_params_login[:password])
+      # Create a token if password is correct
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: token }, status: :ok
+    else
+      render json: { error: 'Username Or Password is incorrect' }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+
+  def user_params_login
+    params.require(:user).permit(:username, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resource :users, only: [:create]
+  resource :users, only: [:create] do
+    member do
+      post :register
+      post :login
+    end
+  end
 end
