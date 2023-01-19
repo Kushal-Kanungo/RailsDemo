9d31aa0e4350aa6cb0589d4fd4712b34fe5eb541
Kushal <kushal.kanungo@metacube.com>
2023-01-19 12:03:12 +0530

    Added decode_token method in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,21 @@ class ApplicationController < ActionController::API
   def encode_token(payload)
     JWT.encode(payload, 'secret')
   end
+
+  def decode_token
+    # Autherization : Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMTIzIn0.AOXUao_6a_LbIcwkaZU574fPqvW6mPvHhwKC7Fatuws
+    unless auth_header
+      auth_header = # request.headers['Authorization']
+      return
+    end
+
+    # extract the token
+    token = auth_header.split(' ')[1]
+    begin
+      JWT.decode(token, 'secret', true, algorithm: 'HS256')
+      # It will return like this : ? [{"user_id"=>"123"}, {"alg"=>"HS256"}]
+    rescue JWT::DecodeError
+      nil
+    end
+  end
 end
