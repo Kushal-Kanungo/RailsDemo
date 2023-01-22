a3b617dc62efa75d5a4a27bcd6f336e117879dd8
Kushal <kushal.kanungo@metacube.com>
2023-01-23 00:28:57 +0530

    Created Action and View of our mailer

--- app/mailers/crud_notification_mailer.rb
+++ app/mailers/crud_notification_mailer.rb
@@ -1,5 +1,10 @@
 class CrudNotificationMailer < ApplicationMailer
-  def create_notification; end
+  def create_notification(object)
+    @object = object
+    @object_count = object.class.count
+
+    mail to: 'admin@example.com', subject: "A new entry for #{object.class} has been created"
+  end
 
   def update_notification; end
 
